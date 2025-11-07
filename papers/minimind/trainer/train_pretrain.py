18e92c1716919a6ff4b3a54689078f575fa78894
webzuo <webzuo02@gmail.com>
2025-11-07 19:01:13 +0800

    the process of training is already completed

--- papers/minimind/trainer/train_pretrain.py
+++ papers/minimind/trainer/train_pretrain.py
@@ -32,10 +32,9 @@ def train_epoch(epoch, loader, iters, start_step=0, wandb=None):
 
         X = X.to(args.device)
         Y = Y.to(args.device)
-
-
         loss_mask = loss_mask.to(args.device)
 
+        # cosine warmup
         lr = get_lr(epoch * iters + step, args.epochs * iters, args.learning_rate)
         
         for param_group in optimizer.param_groups:
